b461820a5e2c874324156dea2c6ca77e78b8030d
Ziaunys <zounes99@gmail.com>
2013-08-11 15:47:00 -0700

    Fixing lumerbjack template and ensuring that lumberjack ssl cert path exists.

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -138,7 +138,6 @@ define lumberjack::instance(
     service { "lumberjack-${name}":
       ensure     => $service_ensure,
       enable     => $service_enable,
-      name       => $lumberjack::params::service_name,
       hasstatus  => $lumberjack::params::service_hasstatus,
       hasrestart => $lumberjack::params::service_hasrestart,
       pattern    => $lumberjack::params::service_pattern,
@@ -150,6 +149,9 @@ define lumberjack::instance(
 
   }
 
+  file { "/etc/lumberjack":
+    ensure => directory,
+  }
 
   file { "/etc/lumberjack/${name}":
     ensure => directory,
